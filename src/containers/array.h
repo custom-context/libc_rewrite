69fde42c393728ae45a1a9372ab96da28490d823
Konstantin S <turnever@gmail.com>
2022-11-02 07:08:20 +0600

    feat containers/array: define macro helpers for array, wrap array functions/struct by macro helpers & make declarations & definitions static

--- src/containers/array.h
+++ src/containers/array.h
@@ -2,65 +2,78 @@
 
 #include <construct/numeric_helpers.h>
 
-#define DECLARE_ARRAY(TYPE, SIZE)\
-struct array_##TYPE##_##SIZE;\
+#include <utils/macros.h>
+
+#define ARRAY_TYPE_(TYPE, SIZE) CONCAT5(array, _, TYPE, _, SIZE)
+#define ARRAY_TYPE(TYPE, SIZE) ARRAY_TYPE_(TYPE, SIZE)
+
+#define ARRAY_METHOD_(TYPE, SIZE, METHOD) TYPE_METHOD(ARRAY_TYPE(TYPE, SIZE), METHOD)
+#define ARRAY_METHOD(TYPE, SIZE, METHOD) ARRAY_METHOD_(TYPE, SIZE, METHOD)
+
+#define ARRAY_STRUCT_TYPE_(TYPE, SIZE) struct ARRAY_TYPE(TYPE, SIZE)
+#define ARRAY_STRUCT_TYPE(TYPE, SIZE) ARRAY_STRUCT_TYPE_(TYPE, SIZE)
+
+#define DECLARE_ARRAY_(TYPE, SIZE)\
+ARRAY_STRUCT_TYPE(TYPE, SIZE);\
 \
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_at(struct array_##TYPE##_##SIZE* const this);\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_copy_at(struct array_##TYPE##_##SIZE* const this, struct array_##TYPE##_##SIZE const* const source);\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_by_value_at(struct array_##TYPE##_##SIZE* const this, TYPE const* const value);\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_from_buffer_at(struct array_##TYPE##_##SIZE* const this, TYPE const (*buffer)[SIZE]);\
-void array_##TYPE##_##SIZE##_destroy_at(struct array_##TYPE##_##SIZE* const this);\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this);\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_copy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const source);\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_by_value_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const* const value);\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_from_buffer_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const (*buffer)[SIZE]);\
+static void ARRAY_METHOD(TYPE, SIZE, destroy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this);\
 \
-uint array_##TYPE##_##SIZE##_size(struct array_##TYPE##_##SIZE const* const this);\
-TYPE const* array_##TYPE##_##SIZE##_data(struct array_##TYPE##_##SIZE const* const this);\
-TYPE const* array_##TYPE##_##SIZE##_at(struct array_##TYPE##_##SIZE const* const this, uint index);\
-TYPE * array_##TYPE##_##SIZE##_mut_at(struct array_##TYPE##_##SIZE* const this, uint index);\
+static uint ARRAY_METHOD(TYPE, SIZE, size)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this);\
+static TYPE const* ARRAY_METHOD(TYPE, SIZE, data)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this);\
+static TYPE const* ARRAY_METHOD(TYPE, SIZE, at)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this, uint index);\
+static TYPE * ARRAY_METHOD(TYPE, SIZE, mut_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, uint index);\
 \
-typedef struct array_##TYPE##_##SIZE {\
+typedef ARRAY_STRUCT_TYPE(TYPE, SIZE) {\
 /*private:*/\
     TYPE buffer_[SIZE];\
-} array_##TYPE##_##SIZE
+} ARRAY_TYPE(TYPE, SIZE)
+#define DECLARE_ARRAY(TYPE, SIZE) DECLARE_ARRAY_(TYPE, SIZE)
 
-#define IMPLEMENT_ARRAY(TYPE, SIZE)\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_at(struct array_##TYPE##_##SIZE* const this) {\
+#define IMPLEMENT_ARRAY_(TYPE, SIZE)\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this) {\
     for (uint index = 0u; index < SIZE; ++index) {\
-        TYPE##_construct_at(array_##TYPE##_##SIZE##_mut_at(this, index));\
+        TYPE_METHOD(TYPE, construct_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
     return this;\
 }\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_copy_at(struct array_##TYPE##_##SIZE* const this, struct array_##TYPE##_##SIZE const* const source) {\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_copy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const source) {\
     for (uint index = 0u; index < SIZE; ++index) {\
-        TYPE##_construct_copy_at(array_##TYPE##_##SIZE##_mut_at(this, index), array_##TYPE##_##SIZE##_at(source, index));\
+        TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), ARRAY_METHOD(TYPE, SIZE, at)(source, index));\
     }\
     return this;\
 }\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_by_value_at(struct array_##TYPE##_##SIZE* const this, TYPE const* const value) {\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_by_value_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const* const value) {\
     for (uint index = 0u; index < SIZE; ++index) {\
-        TYPE##_construct_copy_at(array_##TYPE##_##SIZE##_mut_at(this, index), value);\
+        TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), value);\
     }\
     return this;\
 }\
-struct array_##TYPE##_##SIZE* array_##TYPE##_##SIZE##_construct_from_buffer_at(struct array_##TYPE##_##SIZE* const this, TYPE const (*buffer)[SIZE]){\
+static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_from_buffer_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const (*buffer)[SIZE]){\
     for (uint index = 0u; index < SIZE; ++index) {\
-        TYPE##_construct_copy_at(array_##TYPE##_##SIZE##_mut_at(this, index), buffer[index]);\
+        TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), buffer[index]);\
     }\
     return this;\
 }\
-void array_##TYPE##_##SIZE##_destroy_at(struct array_##TYPE##_##SIZE* const this){\
+static void ARRAY_METHOD(TYPE, SIZE, destroy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this){\
     for (uint index = 0u; index < SIZE; ++index) {\
-        TYPE##_destroy_at(array_##TYPE##_##SIZE##_mut_at(this, index));\
+        TYPE_METHOD(TYPE, destroy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
 }\
 \
-uint array_##TYPE##_##SIZE##_size(struct array_##TYPE##_##SIZE const* const this) {\
+static uint ARRAY_METHOD(TYPE, SIZE, size)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
     return SIZE;\
 }\
-TYPE const* array_##TYPE##_##SIZE##_data(struct array_##TYPE##_##SIZE const* const this) {\
+static TYPE const* ARRAY_METHOD(TYPE, SIZE, data)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
     return this->buffer_;\
 }\
-TYPE * array_##TYPE##_##SIZE##_mut_at(struct array_##TYPE##_##SIZE* const this, uint index) {\
+static TYPE * ARRAY_METHOD(TYPE, SIZE, mut_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, uint index) {\
     return this->buffer_ + index;\
 }\
-TYPE const* array_##TYPE##_##SIZE##_at(struct array_##TYPE##_##SIZE const* const this, uint index) {\
+static TYPE const* ARRAY_METHOD(TYPE, SIZE, at)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this, uint index) {\
     return this->buffer_ + index;\
 }
+#define IMPLEMENT_ARRAY(TYPE, SIZE) IMPLEMENT_ARRAY_(TYPE, SIZE)
