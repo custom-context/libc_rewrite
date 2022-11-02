63ffa842d9dae1b6f4a5b621b850e77667c03f7c
Konstantin S <turnever@gmail.com>
2022-11-02 07:15:52 +0600

    feat tests: add macro helpers for tests writing

---
+++ src/tests/utils/macros.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <utils/macros.h>
+
+#ifdef ENABLE_TESTS
+
+#include <stdio.h>
+
+#define DECLARE_TYPE_TESTS(TYPE) extern void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define IMPLEMENT_TYPE_TESTS(TYPE) void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define DEFINE_TYPE_TESTS(TYPE) inline static void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define EXECUTE_TYPE_TESTS_(TYPE) FUNCTION_FOR_TYPE(test, TYPE)(""#TYPE)
+#define EXECUTE_TYPE_TESTS(TYPE) EXECUTE_TYPE_TESTS_(TYPE)
+
+#define TEST_BLOCK_(NAME)\
+for (char* TEST_BLOCK_NAME = ""#NAME; TEST_BLOCK_NAME != NULL; TEST_BLOCK_NAME = NULL)\
+for (size_t TEST_BLOCK_COUNTER = 0; TEST_BLOCK_COUNTER != 1; TEST_BLOCK_COUNTER = 1)
+#define TEST_BLOCK(NAME) TEST_BLOCK_(NAME)
+
+#define ASSERT_(EXPR)\
+if ((char)(EXPR)) {} else {\
+    printf("[%s][%zu]: at " __FILE__ ":%d: Assertion `" #EXPR "` failed\n", TEST_BLOCK_NAME, ++TEST_BLOCK_COUNTER, __LINE__);\
+}
+#define ASSERT(EXPR) ASSERT_(EXPR)
+
+#else
+
+#include <stddef.h>
+
+#define DECLARE_TYPE_TESTS(TYPE) extern void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define IMPLEMENT_TYPE_TESTS(TYPE) void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define DEFINE_TYPE_TESTS(TYPE) inline static void FUNCTION_FOR_TYPE(test, TYPE)(const char* TYPE_NAME)
+#define EXECUTE_TYPE_TESTS(TYPE)
+#define TEST_BLOCK(NAME)\
+for (char* TEST_BLOCK_NAME; 0;)\
+for (size_t TEST_BLOCK_COUNTER; 0;)
+
+#define ASSERT(EXPR) {}
+
+#endif
