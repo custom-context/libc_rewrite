7b0842fbc97607032ccc085ce569cbf0a8730b5a
Konstantin S <turnever@gmail.com>
2022-11-02 08:25:04 +0600

    feat containers/array: add assertions over this pointer

--- src/containers/array.h
+++ src/containers/array.h
@@ -35,45 +35,54 @@ typedef ARRAY_STRUCT_TYPE(TYPE, SIZE) {\
 
 #define IMPLEMENT_ARRAY_(TYPE, SIZE)\
 ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
     return this;\
 }\
 ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_copy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const source) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), ARRAY_METHOD(TYPE, SIZE, at)(source, index));\
     }\
     return this;\
 }\
 ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_by_value_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const* const value) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), value);\
     }\
     return this;\
 }\
 ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_from_buffer_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const (*buffer)[SIZE]){\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), buffer[index]);\
     }\
     return this;\
 }\
 void ARRAY_METHOD(TYPE, SIZE, destroy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this){\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, destroy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
 }\
 \
 uint ARRAY_METHOD(TYPE, SIZE, size)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
+    ASSERT(this);\
     return SIZE;\
 }\
 TYPE const* ARRAY_METHOD(TYPE, SIZE, data)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
+    ASSERT(this);\
     return this->buffer_;\
 }\
 TYPE * ARRAY_METHOD(TYPE, SIZE, mut_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, uint index) {\
+    ASSERT(this);\
     return this->buffer_ + index;\
 }\
 TYPE const* ARRAY_METHOD(TYPE, SIZE, at)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this, uint index) {\
+    ASSERT(this);\
     return this->buffer_ + index;\
 }
 #define IMPLEMENT_ARRAY(TYPE, SIZE) IMPLEMENT_ARRAY_(TYPE, SIZE)
@@ -97,45 +106,54 @@ typedef ARRAY_STRUCT_TYPE(TYPE, SIZE) {\
 } ARRAY_TYPE(TYPE, SIZE);\
 \
 static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
     return this;\
 }\
 static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_copy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const source) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), ARRAY_METHOD(TYPE, SIZE, at)(source, index));\
     }\
     return this;\
 }\
 static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_by_value_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const* const value) {\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), value);\
     }\
     return this;\
 }\
 static ARRAY_STRUCT_TYPE(TYPE, SIZE)* ARRAY_METHOD(TYPE, SIZE, construct_from_buffer_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, TYPE const (*buffer)[SIZE]){\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, construct_copy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index), buffer[index]);\
     }\
     return this;\
 }\
 static void ARRAY_METHOD(TYPE, SIZE, destroy_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this){\
+    ASSERT(this);\
     for (uint index = 0u; index < SIZE; ++index) {\
         TYPE_METHOD(TYPE, destroy_at)(ARRAY_METHOD(TYPE, SIZE, mut_at)(this, index));\
     }\
 }\
 \
 static uint ARRAY_METHOD(TYPE, SIZE, size)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
+    ASSERT(this);\
     return SIZE;\
 }\
 static TYPE const* ARRAY_METHOD(TYPE, SIZE, data)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this) {\
+    ASSERT(this);\
     return this->buffer_;\
 }\
 static TYPE * ARRAY_METHOD(TYPE, SIZE, mut_at)(ARRAY_STRUCT_TYPE(TYPE, SIZE)* const this, uint index) {\
+    ASSERT(this);\
     return this->buffer_ + index;\
 }\
 static TYPE const* ARRAY_METHOD(TYPE, SIZE, at)(ARRAY_STRUCT_TYPE(TYPE, SIZE) const* const this, uint index) {\
+    ASSERT(this);\
     return this->buffer_ + index;\
 }
 #define DEFINE_ARRAY(TYPE, SIZE) DEFINE_ARRAY_(TYPE, SIZE)
