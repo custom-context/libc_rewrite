e1d5e4dc3b1127137fddd2b5b7da304dbdb1552e
Konstantin S <turnever@gmail.com>
2022-11-03 21:33:20 +0600

    feat tests/containers/string: add tests over `pop_back` & `shrink_to_fit` functions

--- src/tests/containers/string/string.c
+++ src/tests/containers/string/string.c
@@ -107,6 +107,63 @@ IMPLEMENT_TYPE_TESTS(string) {
         STRING_METHOD(destroy_at)(&str);
     }
 
+    TEST_BLOCK("`pop_back`") {
+        STRING_TYPE() str;
+        STRING_METHOD(construct_at)(&str);
+
+        CHECK(STRING_METHOD(size)(&str) == 0);
+        CHECK(STRING_METHOD(capacity)(&str) > 0);
+
+        for (size_t i = 0u; i < 10 + 26 + 26; ++i) {
+            char value;
+            if (i < 10) {
+                value = i + '0';
+                STRING_METHOD(push_back)(&str, &value);
+                continue;
+            }
+            if (i < 10 + 26) {
+                value = i - 10 + 'a';
+                STRING_METHOD(push_back)(&str, &value);
+                continue;
+            }
+            if (i < 10 + 26 + 26) {
+                value =  i - 36 + 'A';
+                STRING_METHOD(push_back)(&str, &value);
+                continue;
+            }
+        }
+        CHECK(STRING_METHOD(size)(&str) == 10 + 26 + 26);
+        CHECK(STRING_METHOD(capacity)(&str) >= STRING_METHOD(size)(&str));
+        CHECK(!str.is_stack_allocated_);
+        
+        for (size_t i = 0u; i < STRING_METHOD(size)(&str); ++i) {
+            if (i < 10) {
+                CHECK(*STRING_METHOD(at)(&str, i) == i + '0');
+                continue;
+            }
+            if (i < 10 + 26) {
+                CHECK(*STRING_METHOD(at)(&str, i) == i - 10 + 'a');
+                continue;
+            }
+            if (i < 10 + 26 + 26) {
+                CHECK(*STRING_METHOD(at)(&str, i) == i - (10 + 26) + 'A');
+                continue;
+            }
+        }
+
+        STRING_TYPE() str2;
+        STRING_METHOD(construct_copy_at)(&str2, &str);
+
+        for (size_t i = 0u; i < STRING_METHOD(size)(&str); ++i) {
+            STRING_METHOD(pop_back)(&str2);
+            CHECK(STRING_METHOD(size)(&str2) == STRING_METHOD(size)(&str) - 1 - i);
+            CHECK(!utils__string__compare_string_char_with_buffer(&str2, STRING_METHOD(size)(&str2), STRING_METHOD(data)(&str)));
+        }
+
+        STRING_METHOD(destroy_at)(&str2);
+        STRING_METHOD(destroy_at)(&str);
+    }
+
     TEST_BLOCK("`construct_from_c_string` method without reallocations") {
         char const* const c_string = "str w. len < 16";
         size_t const c_string_length = utils__string__string_char_length(c_string);
@@ -134,6 +191,52 @@ IMPLEMENT_TYPE_TESTS(string) {
 
         CHECK(!utils__string__compare_string_char_with_buffer(&str, c_string_length, c_string));
         CHECK(!str.is_stack_allocated_);
+
+        STRING_METHOD(destroy_at)(&str);
+    }
+
+    TEST_BLOCK("`shrink_to_fit` method without reallocations") {
+        char const* const c_string = "str w. len < 16";
+        size_t const c_string_length = utils__string__string_char_length(c_string);
+
+        STRING_TYPE() str;
+        STRING_METHOD(construct_from_c_string_at)(&str, c_string);
+
+        CHECK(STRING_METHOD(size)(&str) == c_string_length);
+        CHECK(STRING_METHOD(capacity)(&str) >= c_string_length);
+        CHECK(!utils__string__compare_string_char_with_buffer(&str, c_string_length, c_string));
+        CHECK(str.is_stack_allocated_);
+
+        STRING_METHOD(pop_back)(&str);
+        STRING_METHOD(shrink_to_fit)(&str);
+
+        CHECK(STRING_METHOD(size)(&str) == c_string_length - 1);
+        CHECK(STRING_METHOD(capacity)(&str) >= c_string_length - 1);
+        CHECK(!utils__string__compare_string_char_with_buffer(&str, c_string_length - 1, c_string));
+        CHECK(str.is_stack_allocated_);
+    }
+
+    TEST_BLOCK("`shrink_to_fit` method with deallocation") {
+        char const* const c_string = "str w. len gt 16";
+        size_t const c_string_length = utils__string__string_char_length(c_string);
+
+        STRING_TYPE() str;
+        STRING_METHOD(construct_from_c_string_at)(&str, c_string);
+
+        CHECK(STRING_METHOD(size)(&str) == c_string_length);
+        CHECK(STRING_METHOD(capacity)(&str) >= c_string_length);
+
+        CHECK(!utils__string__compare_string_char_with_buffer(&str, c_string_length, c_string));
+        CHECK(!str.is_stack_allocated_);
+
+        //
+        STRING_METHOD(pop_back)(&str);
+        STRING_METHOD(shrink_to_fit)(&str);
+
+        CHECK(STRING_METHOD(size)(&str) == c_string_length - 1);
+        CHECK(STRING_METHOD(capacity)(&str) >= c_string_length - 1);
+        CHECK(!utils__string__compare_string_char_with_buffer(&str, c_string_length - 1, c_string));
+        CHECK(str.is_stack_allocated_);
 
         STRING_METHOD(destroy_at)(&str);
     }
