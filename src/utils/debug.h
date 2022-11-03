b4d744835387090ceaa6f633761f6d99907c4536
Konstantin S <turnever@gmail.com>
2022-11-03 21:30:28 +0600

    feat utils: change usage of printf to puts in LOG_DEBUG macro

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -2,7 +2,7 @@
 
 #ifdef DEBUG
     #include <stdio.h>
-    #define LOG_DEBUG(MSG) printf(MSG)
+    #define LOG_DEBUG(MSG) puts(MSG)
     #define LOG_DEBUG_FORMAT(FORMAT_STRING, ...) printf(FORMAT_STRING, __VA_ARGS__)
 #else
     #define LOG_DEBUG(MSG) (MSG, 0)
