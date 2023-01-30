0fa2c8f81ca5f8b1ee2156ddc18290153abc6c12
Konstantin S <turnever@gmail.com>
2023-01-30 11:14:10 +1000

    feat fs/file: add utf's non-bom and bom encoding checkers

--- src/fs/file/encoding.h
+++ src/fs/file/encoding.h
@@ -13,12 +13,18 @@
 // article: http://unicode.org/faq/utf_bom.html#BOM
 
 typedef enum NAMESPACE_FS_FILE(UTF_ENCODING) {
-    UTF_ENCODING_ENUM_VALUE(UTF32_BE), // 00 00 FE FF    UTF-32, big-endian
-    UTF_ENCODING_ENUM_VALUE(UTF32_LE), // FF FE 00 00    UTF-32, little-endian
-    UTF_ENCODING_ENUM_VALUE(UTF16_BE), // FE FF          UTF-16, big-endian
-    UTF_ENCODING_ENUM_VALUE(UTF16_LE), // FF FE          UTF-16, little-endian
-    UTF_ENCODING_ENUM_VALUE(UTF8_BOM), // EF BB BF       UTF-8, BOM
-    UTF_ENCODING_ENUM_VALUE(UTF8),     // ELSE           UTF-8
+    // BOM encoding
+    UTF_ENCODING_ENUM_VALUE(UTF32_BE_BOM), // 00 00 FE FF    UTF-32, big-endian, BOM
+    UTF_ENCODING_ENUM_VALUE(UTF32_LE_BOM), // FF FE 00 00    UTF-32, little-endian, BOM
+    UTF_ENCODING_ENUM_VALUE(UTF16_BE_BOM), // FE FF xx xx    UTF-16, big-endian, BOM
+    UTF_ENCODING_ENUM_VALUE(UTF16_LE_BOM), // FF FE xx xx    UTF-16, little-endian, BOM
+    UTF_ENCODING_ENUM_VALUE(UTF8_BOM),     // EF BB BF xx    UTF-8, BOM
+    // non-BOM encoding (JSON files for example have non-BOM format)
+    UTF_ENCODING_ENUM_VALUE(UTF32_BE),     // 00 00 00 xx    UTF-32, big-endian
+    UTF_ENCODING_ENUM_VALUE(UTF32_LE),     // xx 00 00 00    UTF-32, little-endian
+    UTF_ENCODING_ENUM_VALUE(UTF16_BE),     // 00 xx xx xx    UTF-16, big-endian
+    UTF_ENCODING_ENUM_VALUE(UTF16_LE),     // xx 00 xx xx    UTF-16, little-endian
+    UTF_ENCODING_ENUM_VALUE(UTF8)          // xx xx xx xx    UTF-8
 } NAMESPACE_FS_FILE(UTF_ENCODING);
 
 enum NAMESPACE_FS_FILE(UTF_ENCODING) NAMESPACE_FS_FILE(get_encoding)(uint const header_bytes_size, char const* const header_bytes);
