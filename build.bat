30487ad421ea039380e25192ec99486153304f1a
Konstantin S <turnever@gmail.com>
2023-02-17 04:39:06 +1000

    feat windows: add build.bat script for project building under windows

---
+++ build.bat
@@ -0,0 +1,8 @@
+@REM change directory to 'build.bat' file directory (project directory)
+cd /D "%~dp0"
+@REM go to sub-directory 'build'
+cd .\build
+@REM make project using clang-cl compiler
+cmake -T ClangCL ..
+@REM build project in Debug mode
+cmake --build .
