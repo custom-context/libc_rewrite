3b918f84c9a0cf44a777372b9922eb5cc59b5634
Konstantin S <turnever@gmail.com>
2023-02-25 02:19:27 +1000

    feat containers: remove redundant memory header from containers/common.h

--- src/containers/common.h
+++ src/containers/common.h
@@ -3,6 +3,5 @@
 #include <construct/numeric_helpers.h>
 #include <utils/macros.h>
 #include <utils/debug.h>
-#include <memory/memory.h>
 
 #define NAMESPACE_CONTAINERS(NAME) CONCAT3(containers, __, NAME)
