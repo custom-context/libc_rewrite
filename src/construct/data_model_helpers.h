1b56b01afb18aceedef46ed7195a512a835a9803
Konstantin S <turnever@gmail.com>
2023-03-11 02:09:20 +1000

    feat construct: add data_model_helpers unit for delcaration/implementation construction/destruction functions via numeric_helpers macro helpers for `size_t` & `ptrdiff_t` types

---
+++ src/construct/data_model_helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "numeric_helpers.h"
+
+#include <stdint.h>
+
+DECLARE_CONSTRUCTORS_AND_DESTRUCTORS_FOR_NUMERIC_TYPE(size_t);
+DECLARE_CONSTRUCTORS_AND_DESTRUCTORS_FOR_NUMERIC_TYPE(ptrdiff_t);
