5cabf97b87d9623fb9280a07f9d69c49c82a8883
Konstantin S <turnever@gmail.com>
2023-03-11 02:36:05 +1000

    feat utils/format: replace usage of vector by dynamic-array & fix source due pedantic compiler option

--- src/utils/format.c
+++ src/utils/format.c
@@ -4,7 +4,7 @@
 #include <utils/string/numeric_type_to_string_impl.h>
 
 #include <utils/string/helpers.h>
-#include <containers/dynamic/vector/vector.h>
+#include <containers/dynamic/array/array.h>
 
 #include <utils/macros.h>
 
@@ -25,75 +25,94 @@ typedef struct LOCAL_NAMESPACE(pattern) {
 } LOCAL_NAMESPACE(pattern);
 #define PATTERN_FN(FN_NAME) STRUCT_FN(LOCAL_NAMESPACE(pattern), FN_NAME)
 
-struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_at)(struct LOCAL_NAMESPACE(pattern) * const this) {
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_at)(struct LOCAL_NAMESPACE(pattern) * const this) {
     ASSERT(this);
     return this;
 }
-struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_copy_at)(struct LOCAL_NAMESPACE(pattern) * const this, struct LOCAL_NAMESPACE(pattern) const* const src) {
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_copy_at)(struct LOCAL_NAMESPACE(pattern) * const this, struct LOCAL_NAMESPACE(pattern) const* const src) {
     ASSERT(this);
     ASSERT(src);
     STRING_METHOD(construct_copy_at)(&this->_string_representation, &src->_string_representation);
     this->_type = src->_type;
     return this;
 }
-void* PATTERN_FN(destroy_at)(struct LOCAL_NAMESPACE(pattern) * const this) {
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_move_at)(struct LOCAL_NAMESPACE(pattern) * const this, struct LOCAL_NAMESPACE(pattern)* const src) {
+    ASSERT(this);
+    ASSERT(src);
+    STRING_METHOD(construct_move_at)(&this->_string_representation, &src->_string_representation);
+    this->_type = src->_type;
+    return this;
+}
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(assign_copy_at)(struct LOCAL_NAMESPACE(pattern) * const this, struct LOCAL_NAMESPACE(pattern) const* const src) {
+    ASSERT(this);
+    ASSERT(src);
+    STRING_METHOD(assign_copy_at)(&this->_string_representation, &src->_string_representation);
+    this->_type = src->_type;
+    return this;
+}
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(assign_move_at)(struct LOCAL_NAMESPACE(pattern) * const this, struct LOCAL_NAMESPACE(pattern)* const src) {
+    ASSERT(this);
+    ASSERT(src);
+    STRING_METHOD(assign_move_at)(&this->_string_representation, &src->_string_representation);
+    this->_type = src->_type;
+    return this;
+}
+static void* PATTERN_FN(destroy_at)(struct LOCAL_NAMESPACE(pattern) * const this) {
     ASSERT(this);
     STRING_METHOD(destroy_at)(&this->_string_representation);
     return this;
 }
 
-struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_with_values_at)(struct LOCAL_NAMESPACE(pattern) * const this,
+static struct LOCAL_NAMESPACE(pattern)* PATTERN_FN(construct_with_values_at)(struct LOCAL_NAMESPACE(pattern) * const this,
     enum LOCAL_NAMESPACE(format_string_pattern_type) type, char const* const pattern_string_representation) {
     STRING_METHOD(construct_from_c_string_at)(&this->_string_representation, pattern_string_representation);
     this->_type = type;
     return this;
 }
 
-DECLARE_VECTOR(LOCAL_NAMESPACE(pattern));
-IMPLEMENT_VECTOR(LOCAL_NAMESPACE(pattern));
+DEFINE_DEFAULT_ALLOCATOR_TYPE(LOCAL_NAMESPACE(pattern));
+DEFINE_DEFAULT_ALLOCATOR_STATIC_METHODS(LOCAL_NAMESPACE(pattern))
+
+DECLARE_DYNAMIC_ARRAY_TYPE(LOCAL_NAMESPACE(pattern));
+IMPLEMENT_DYNAMIC_ARRAY_TYPE_STATIC_METHODS(LOCAL_NAMESPACE(pattern))
 
 typedef struct LOCAL_NAMESPACE(patterns) {
-    struct VECTOR_TYPE(LOCAL_NAMESPACE(pattern)) _patterns;
+    struct DYNAMIC_ARRAY_TYPE(LOCAL_NAMESPACE(pattern)) _patterns;
 } LOCAL_NAMESPACE(patterns);
 #define PATTERNS_FN(FN_NAME) STRUCT_FN(LOCAL_NAMESPACE(patterns), FN_NAME)
 
-struct LOCAL_NAMESPACE(patterns) * PATTERNS_FN(construct_at)(struct LOCAL_NAMESPACE(patterns) * const this) {
-    struct VECTOR_TYPE(LOCAL_NAMESPACE(pattern)) * patterns = &this->_patterns;
+static struct LOCAL_NAMESPACE(patterns) * PATTERNS_FN(construct_at)(struct LOCAL_NAMESPACE(patterns) * const this) {
+    struct DYNAMIC_ARRAY_TYPE(LOCAL_NAMESPACE(pattern)) * patterns = &this->_patterns;
 
-    VECTOR_METHOD(LOCAL_NAMESPACE(pattern), construct_at)(patterns);
+    DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), construct_at)(patterns);
 
     struct LOCAL_NAMESPACE(pattern) pattern;
     { // "%%"
         PATTERN_FN(construct_with_values_at)(&pattern, ESCAPED_PERCENT, "%%");
-        VECTOR_METHOD(LOCAL_NAMESPACE(pattern), push_back)(patterns, &pattern);
-        PATTERN_FN(destroy_at)(&pattern);
+        DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), push_back_by_moving)(patterns, &pattern);
     }
     { // "%c"
         PATTERN_FN(construct_with_values_at)(&pattern, CHARACTER, "%c");
-        VECTOR_METHOD(LOCAL_NAMESPACE(pattern), push_back)(patterns, &pattern);
-        PATTERN_FN(destroy_at)(&pattern);
+        DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), push_back_by_moving)(patterns, &pattern);
     }
     { // "%s"
         PATTERN_FN(construct_with_values_at)(&pattern, NULL_TERMINATED_STRING, "%s");
-        VECTOR_METHOD(LOCAL_NAMESPACE(pattern), push_back)(patterns, &pattern);
-        PATTERN_FN(destroy_at)(&pattern);
+        DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), push_back_by_moving)(patterns, &pattern);
     }
     { // "%d"
         PATTERN_FN(construct_with_values_at)(&pattern, SIGNED_INTEGER_TO_DECIMAL, "%d");
-        VECTOR_METHOD(LOCAL_NAMESPACE(pattern), push_back)(patterns, &pattern);
-        PATTERN_FN(destroy_at)(&pattern);
+        DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), push_back_by_moving)(patterns, &pattern);
     }
     { // "%i"
         PATTERN_FN(construct_with_values_at)(&pattern, SIGNED_INTEGER_TO_DECIMAL, "%i");
-        VECTOR_METHOD(LOCAL_NAMESPACE(pattern), push_back)(patterns, &pattern);
-        PATTERN_FN(destroy_at)(&pattern);
+        DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), push_back_by_moving)(patterns, &pattern);
     }
     return this;
 }
-void* PATTERNS_FN(destroy_at)(struct LOCAL_NAMESPACE(patterns) * const this) {
+static void* PATTERNS_FN(destroy_at)(struct LOCAL_NAMESPACE(patterns) * const this) {
     ASSERT(this);
-    struct VECTOR_TYPE(LOCAL_NAMESPACE(pattern)) * patterns = &this->_patterns;
-    VECTOR_METHOD(LOCAL_NAMESPACE(pattern), destroy_at)(patterns);
+    struct DYNAMIC_ARRAY_TYPE(LOCAL_NAMESPACE(pattern)) * patterns = &this->_patterns;
+    DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), destroy_at)(patterns);
     return this;
 }
 
@@ -101,11 +120,11 @@ typedef struct LOCAL_NAMESPACE(format_string_pattern_occurrence_index) {
     LOCAL_NAMESPACE(pattern) const* _pattern;
     uint _index;
 } LOCAL_NAMESPACE(format_string_pattern_occurrence_index);
-struct LOCAL_NAMESPACE(format_string_pattern_occurrence_index) LOCAL_NAMESPACE(search_pattern_occurence)(char const* const format_string, LOCAL_NAMESPACE(patterns) const* const patterns) {
+static struct LOCAL_NAMESPACE(format_string_pattern_occurrence_index) LOCAL_NAMESPACE(search_pattern_occurence)(char const* const format_string, LOCAL_NAMESPACE(patterns) const* const patterns) {
     LOCAL_NAMESPACE(format_string_pattern_occurrence_index) result = {._pattern = NULL, ._index = 0u};
     for (; format_string[result._index] != '\0'; ++result._index) {
-        for (uint vec_index = 0u; vec_index < VECTOR_METHOD(LOCAL_NAMESPACE(pattern), size)(&patterns->_patterns); ++vec_index) {
-            result._pattern = VECTOR_METHOD(LOCAL_NAMESPACE(pattern), at)(&patterns->_patterns, vec_index);
+        for (uint vec_index = 0u; vec_index < DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), size)(&patterns->_patterns); ++vec_index) {
+            result._pattern = DYNAMIC_ARRAY_METHOD(LOCAL_NAMESPACE(pattern), at)(&patterns->_patterns, vec_index);
             if (!NAMESPACE_UTILS_STRING(COMPARE_FUNCTION(STRING_TYPE(), buffer))(
                 &result._pattern->_string_representation,
                 STRING_METHOD(size)(&result._pattern->_string_representation),
@@ -152,7 +171,7 @@ struct STRING_TYPE() NAMESPACE_UTILS(va_format)(char const* const format_string,
                 STRING_METHOD(push_back)(&formatted_string, &value);
             } break;
             case CHARACTER: {
-                char value = va_arg(args, int);
+                char value = (char)va_arg(args, int);
                 STRING_METHOD(push_back)(&formatted_string, &value);
             } break;
             case NULL_TERMINATED_STRING: {
