dc5d65514b2f79e0e864e0b13769820184180a1b
Konstantin S <turnever@gmail.com>
2023-03-27 05:25:51 +1000

    feat primitives: add TCHAR -> winchar alias & OS_CHAR_TYPE macro helper

--- src/primitives/characters_types_aliases.h
+++ src/primitives/characters_types_aliases.h
@@ -1,9 +1,22 @@
 #pragma once
 
 #include <stdint.h>
+#include <wchar.h>
 
 typedef uint8_t char8;
 typedef uint16_t char16;
 typedef uint32_t char32;
 typedef uint64_t char64;
 
+typedef wchar_t wchar;
+
+#if defined(_WIN32)
+// windows character type
+    #include <tchar.h>
+    typedef TCHAR winchar;
+
+    #define OS_CHAR_TYPE winchar
+#else
+// utf-8 oriented character type
+    #define OS_CHAR_TYPE char
+#endif
