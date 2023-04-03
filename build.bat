0d644fbf4a918c60d420258489779dfab9c97602
Konstantin S <turnever@gmail.com>
2023-04-04 02:46:11 +1000

    feat windows: change build.bat due tests & main build separation

--- build.bat
+++ build.bat
@@ -1,10 +1,41 @@
+SET argument1=%~1
 @REM change directory to 'build.bat' file directory (project directory)
 cd /D "%~dp0"
-@REM go to sub-directory 'build'
-cd .\build
-@REM make project using clang-cl compiler
-cmake -T ClangCL ..
-@REM build project in Debug mode
-cmake --build .
-@REM go to project-directory
-cd ..
+IF "%argument1%" == "" (
+    GOTO :build
+) 
+IF "%argument1%" == "-t" (
+    GOTO :tests
+)
+IF "%argument1%" == "--tests" (
+    GOTO :tests
+)
+GOTO :eof
+:tests
+    @REM create sub-directory 'test' if it's not exists
+    IF NOT EXIST .\build-tests (
+        mkdir .\build-tests
+    )
+    @REM go to sub-directory 'tests'
+    cd .\build-tests
+    @REM make project using clang-cl compiler
+    cmake -T ClangCL ..\tests
+    @REM build project in Debug mode
+    cmake --build .
+    @REM go to project-directory
+    cd ..
+GOTO :eof
+:build
+    @REM create sub-directory 'test' if it's not exists
+    IF NOT EXIST .\build (
+        mkdir .\build
+    )
+    @REM go to sub-directory 'build'
+    cd .\build
+    @REM make project using clang-cl compiler
+    cmake -T ClangCL ..
+    @REM build project in Debug mode
+    cmake --build .
+    @REM go to project-directory
+    cd ..
+GOTO :eof
