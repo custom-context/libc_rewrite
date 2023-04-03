0c1a5f63f60bdd72741cd8fe5b3b5c3dc3268475
Konstantin S <turnever@gmail.com>
2023-04-04 00:01:33 +1000

    feat tests/main: change exec_tests to execute_test_by_name & refactor main function tests invocation logic

--- src/tests/main.c
+++ src/tests/main.c
@@ -3,6 +3,7 @@
 
 #include <containers/dynamic/string/string.h>
 
+#include <tests/utils/string/raw_string/compare/compare.h>
 #include <tests/utils/format/format.h>
 #include <tests/containers/reference_counted/reference_counted.h>
 #include <tests/concurrent/thread/native_thread.h>
@@ -20,37 +21,58 @@
 #include <tests/network/nb_client/nb_client.h>
 #include <tests/network/nb_server/nb_server.h>
 
-void exec_tests(void);
-
 #if defined(_WIN32)
     #define MAIN _tmain
 #else
     #define MAIN main
 #endif
 
+void execute_test_by_name(OS_CHAR_TYPE const* test_name);
+
 int MAIN(int const argc, OS_CHAR_TYPE const* const argv[]) {
     UNUSED(argc);
     UNUSED(argv);
-    exec_tests();
+    if (argc < 2) {
+        NAMESPACE_UTILS(format_print)("Count of arguments less than 2. provided argc: %d", argc);
+        return 1;
+    }
+    execute_test_by_name(OS_STRING_LITERAL("raw_string_compare"));
+    for (usize index = 0u; index < (usize)(argc - 1); ++index) {
+        execute_test_by_name(argv[index + 1u]);
+    }
 
     return 0;
 }
 
-void exec_tests(void) {
-    EXECUTE_TYPE_TESTS(format);
-    EXECUTE_TYPE_TESTS(reference_counted);
-    EXECUTE_TYPE_TESTS(native_thread);
-    EXECUTE_TYPE_TESTS(array);
-    EXECUTE_TYPE_TESTS(dynamic_array);
-    EXECUTE_TYPE_TESTS(rb_tree_set);
-    EXECUTE_TYPE_TESTS(rb_tree_map);
-    EXECUTE_TYPE_TESTS(string);
-    EXECUTE_TYPE_TESTS(result);
-    EXECUTE_TYPE_TESTS(encoding);
-    EXECUTE_TYPE_TESTS(socket);
-    EXECUTE_TYPE_TESTS(address_info);
-    EXECUTE_TYPE_TESTS(client);
-    EXECUTE_TYPE_TESTS(server);
-    EXECUTE_TYPE_TESTS(nb_client);
-    EXECUTE_TYPE_TESTS(nb_server);
+#include "string.h"
+
+#define EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(TEST_NAME_VAR, TEST_NAME)\
+if (!RAW_STRING_FUNCTION(OS_CHAR_TYPE, compare)(TEST_NAME_VAR, OS_STRING_LITERAL(#TEST_NAME))) {\
+    EXECUTE_TYPE_TESTS(TEST_NAME);\
+} else {}\
+
+void execute_test_by_name(OS_CHAR_TYPE const* test_name) {
+    #if defined(_WIN32)
+    if (!_tcscmp(test_name, OS_STRING_LITERAL("raw_string_compare")))
+    #else
+    if (!strcmp(test_name, OS_STRING_LITERAL("raw_string_compare")))
+    #endif
+    {
+        EXECUTE_TYPE_TESTS(raw_string_compare);
+    }
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, format)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, reference_counted)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, native_thread)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, array)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, dynamic_array)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, rb_tree_set)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, rb_tree_map)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, string)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, result)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, encoding)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, socket)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, address_info)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, client)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, nb_client)
+    EXECUTE_TYPE_TESTS_ON_EQUAL_TEST_NAME(test_name, nb_server)
 }
