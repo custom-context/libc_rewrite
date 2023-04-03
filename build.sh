ce44d6680eff19f481c8ba0e1a9123e9bcfa2341
Konstantin S <turnever@gmail.com>
2023-04-04 03:00:19 +1000

    feat linux: update build.sh due tests & main library files separation

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-mkdir -p build
-cd build
-cmake ..
-cmake --build .
-make
+if [[ -z "$1" ]]; then
+    cd "$(dirname "$0")"
+    mkdir -p build
+    cd build
+    cmake ..
+    cmake --build .
+    make
+    exit 0
+fi
+if [[ $1 = "--tests" ] || [ $1 = "-t" ]]; then
+    cd "$(dirname "$0")"
+    mkdir -p build-tests
+    cd build-tests
+    cmake ../tests
+    cmake --build .
+    make
+    exit 0
+fi
