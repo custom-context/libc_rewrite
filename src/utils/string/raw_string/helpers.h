05d9b472923a51eb1c6c3304592167f860ad2065
Konstantin S <turnever@gmail.com>
2023-04-12 21:45:27 +1000

    feat string: move OS_STRING_LITERAL macro from containers/dynamic/string/string.h to utils/string/raw_string/helpers.h

--- src/utils/string/raw_string/helpers.h
+++ src/utils/string/raw_string/helpers.h
@@ -2,3 +2,10 @@
 
 #include "length.h"
 #include "compare.h"
+
+// define OS_STRING_LITERAL
+#if defined(_WIN32)
+    #define OS_STRING_LITERAL(STR) _TEXT(STR)
+#else
+    #define OS_STRING_LITERAL(STR) STR
+#endif
