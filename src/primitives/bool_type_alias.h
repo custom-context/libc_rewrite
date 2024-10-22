f5886d540e8e3e261341396ad872c915d8d452c4
Konstantin S <turnever@gmail.com>
2024-10-22 13:44:35 +0300

    fix bool compatibility: add check for lang version for definition of bool/true/false macros

--- src/primitives/bool_type_alias.h
+++ src/primitives/bool_type_alias.h
@@ -1,5 +1,18 @@
 #pragma once
 
+// C2x integrates these keywords: bool, true, false
+#if __STDC_VERSION__ < 202311L
+
+#ifndef bool
 typedef unsigned char bool;
+#endif
+
+#ifndef true
 #define true ((bool)1u)
+#endif
+
+#ifndef false
 #define false ((bool)0u)
+#endif
+
+#endif
