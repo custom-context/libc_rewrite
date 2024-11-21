447e6b77e3adde8d46df81e88dd7d51dfe239967
Konstantin S <turnever@gmail.com>
2024-11-22 02:14:50 +0300

    refactor memory/native: add aligned storage manipulation helpers

--- src/memory/native/native.c
+++ src/memory/native/native.c
@@ -2,10 +2,8 @@
 
 #include <utils/macros.h>
 #include <stdlib.h>
-
-#if defined(_WIN32)
-    #include <malloc.h>
-#endif
+#include <malloc.h>
+#include <string.h>
 
 void* NAMESPACE_MEMORY_NATIVE(malloc)(usize size) {
     return malloc(size);
@@ -18,3 +16,34 @@ void* NAMESPACE_MEMORY_NATIVE(realloc)(void* pointer, usize new_size) {
 void NAMESPACE_MEMORY_NATIVE(free)(void* pointer) {
     free(pointer);
 }
+
+#if defined(_WIN32)
+    void* NAMESPACE_MEMORY_NATIVE(aligned_alloc)(usize alignment, usize size) {
+        return _aligned_malloc(size, alignment);
+    }
+    void* NAMESPACE_MEMORY_NATIVE(aligned_realloc)(void* pointer, usize alignment, usize new_size) {
+        return _aligned_realloc(pointer, new_size, alignment);
+    }
+    void NAMESPACE_MEMORY_NATIVE(aligned_free)(void* pointer) {
+        _aligned_free(pointer);
+    }
+#else
+    void* NAMESPACE_MEMORY_NATIVE(aligned_alloc)(usize alignment, usize size) {
+        return memalign(alignment, size);
+    }
+    void* NAMESPACE_MEMORY_NATIVE(aligned_realloc)(void* pointer, usize alignment, usize new_size) {
+        void* const reallocated_storage = realloc(pointer, new_size);
+        // if reallocated memory is NULL or it has the same alignment => return it
+        if (!reallocated_storage || NAMESPACE_MEMORY_NATIVE(is_aligned)(reallocated_storage, alignment)) {
+            return reallocated_storage;
+        }
+        // else allocate new chunk, copy data & free previous chunk
+        void* const new_aligned_storage = NAMESPACE_MEMORY_NATIVE(aligned_alloc)(alignment, new_size);
+        memcpy(new_aligned_storage, reallocated_storage, new_size);
+        NAMESPACE_MEMORY_NATIVE(aligned_free)(reallocated_storage);
+        return new_aligned_storage;
+    }
+    void NAMESPACE_MEMORY_NATIVE(aligned_free)(void* pointer) {
+        free(pointer);
+    }
+#endif
--- src/memory/native/native.h
+++ src/memory/native/native.h
@@ -2,9 +2,18 @@
 
 #include <memory/common.h>
 #include <primitives/data_model_types_aliases.h>
+#include <primitives/bool_type_alias.h>
 
 #define NAMESPACE_MEMORY_NATIVE(NAME) NAMESPACE_MEMORY(CONCAT3(native, __, NAME))
 
 void* NAMESPACE_MEMORY_NATIVE(malloc)(usize size);
 void* NAMESPACE_MEMORY_NATIVE(realloc)(void* pointer, usize new_size);
 void NAMESPACE_MEMORY_NATIVE(free)(void* pointer);
+
+inline static bool NAMESPACE_MEMORY_NATIVE(is_aligned)(const void *restrict pointer, size_t byte_count) {
+    return !((uintptr)pointer % byte_count);
+}
+
+void* NAMESPACE_MEMORY_NATIVE(aligned_alloc)(usize alignment, usize size);
+void* NAMESPACE_MEMORY_NATIVE(aligned_realloc)(void* pointer, usize alignment, usize new_size);
+void NAMESPACE_MEMORY_NATIVE(aligned_free)(void* pointer);
