accd1a1e1aaa45533569e6b0b617fd4ffcbde360
Konstantin S <turnever@gmail.com>
2026-03-06 16:41:34 +0300

    refactor construct: replace DECLARE_DEFAULT_LIFECYCLE_METHODS_WITH_MODIFIER to DECLARE_NUMERIC_TYPE_DEFAULT_LIFECYCLE_METHODS_WITH_MODIFIER

--- src/construct/bool_helpers.h
+++ src/construct/bool_helpers.h
@@ -3,4 +3,4 @@
 #include "numeric_helpers.h"
 #include <primitives/bool_type_alias.h>
 
-DECLARE_DEFAULT_LIFECYCLE_METHODS_WITH_MODIFIER(, bool);
+DECLARE_NUMERIC_TYPE_DEFAULT_LIFECYCLE_METHODS_WITH_MODIFIER(, bool);
